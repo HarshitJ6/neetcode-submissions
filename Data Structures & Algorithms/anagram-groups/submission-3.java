07c79a15cb7c1edfcf26f22f573d1b74fce618e4
Harshit <64804307+HarshitJ6@users.noreply.github.com>
2026-08-14 21:42:16 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> resMap = new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String anagramCode = generateAnagramCode(strs[i]);
+            if(resMap.containsKey(anagramCode)){
+                List<String> list = resMap.get(anagramCode);
+                list.add(strs[i]);
+                resMap.put(anagramCode, list);
+            }else{
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                resMap.put(anagramCode, list);
+            }
+        }
+        //System.out.println(resMap);
+        List<List<String>> result = new ArrayList<>();
+        for(Map.Entry x : resMap.entrySet()){
+            result.add((ArrayList)x.getValue());
+        }
+
+        return result;
+    }
+
+    public String generateAnagramCode(String str){
+        int[] carr = new int[26];
+        StringBuilder res = new StringBuilder();
+        char[] schar = str.toCharArray();
+        for(int i=0;i<schar.length;i++){
+            carr[schar[i] - 'a']++;
+        }
+
+        for(int x : carr){
+            res.append("*");
+            res.append(x);
+        }
+        res.append("*");
+
+        return res.toString();
+    }
+}
